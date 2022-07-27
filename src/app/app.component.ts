2a2b62fd9d92641390431bef0945edeeffd25c98
Vikram Singh Rawat <36297291+Vikram2921@users.noreply.github.com>
2022-07-27 21:35:19 +0530

    started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FirebaseService} from "./service/firebase.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'json-server';
+  constructor(private fs:FirebaseService) {
+    fs.createNewId();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJtklPD9tw_xjBt3xWAbvBKEPwQGKie_E",
+  authDomain: "jsondb-56fcd.firebaseapp.com",
+  projectId: "jsondb-56fcd",
+  storageBucket: "jsondb-56fcd.appspot.com",
+  messagingSenderId: "1081192472138",
+  appId: "1:1081192472138:web:cfb12801fe53b67dc210b3",
+  measurementId: "G-FG7K30NTW8"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +22,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
